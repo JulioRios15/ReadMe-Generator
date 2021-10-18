53402c43ddbde3edfff171028c202ada1b46b9c3
Julio Rios <rios.julio.david@outlook.com>
2021-10-18 13:43:29 -0400

    implementing more license badges

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -10,7 +10,22 @@ function renderLicenseBadge(license) {
       return `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)]`
     case 'Apache-2.0':
       return `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)]`;
-  
+    case 'BSD-2-Clause': 
+      return `[![License](https://img.shields.io/badge/License-BSD%202--Clause-orange.svg)]`;
+    case 'BSD-3-Clause': 
+      return `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)]`;
+    case 'BSL-1.0':
+      return `[![License](https://img.shields.io/badge/License-Boost%201.0-lightblue.svg)]`;
+    case 'GPL-3.0':
+      return `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)]`;
+    case 'GPL-2.0':
+      return `[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)]`;
+    case 'AGPL-3.0':
+      return `[![License: AGPL v3](https://img.shields.io/badge/License-AGPL%20v3-blue.svg)]`;
+    case 'LGPL-3.0':
+      return `[![License: LGPL v3](https://img.shields.io/badge/License-LGPL%20v3-blue.svg)]`;
+    case 'IPL-1.0':
+      return `[![License: IPL 1.0](https://img.shields.io/badge/License-IPL%201.0-blue.svg)]`
     default:
       return "";
   }
@@ -28,7 +43,22 @@ function renderLicenseLink(license) {
       return `(https://opensource.org/licenses/MIT)`;
     case 'Apache-2.0':
       return `(https://opensource.org/licenses/Apache-2.0)`;
-  
+    case 'BSD-2-Clause': 
+      return `(https://opensource.org/licenses/BSD-2-Clause)`
+    case 'BSD-3-Clause': 
+      return `(https://opensource.org/licenses/BSD-3-Clause)`
+    case 'BSL-1.0':
+      return `(https://www.boost.org/LICENSE_1_0.txt)`;
+    case 'GPL-3.0':
+      return `(https://www.gnu.org/licenses/gpl-3.0)`;
+    case 'GPL-2.0':
+      return `(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html`;
+    case 'AGPL-3.0':
+      return `(https://www.gnu.org/licenses/agpl-3.0`;
+    case 'LGPL-3.0':
+      return `(https://www.gnu.org/licenses/lgpl-3.0)`;
+    case 'IPL-1.0':
+      return `(https://opensource.org/licenses/IPL-1.0)` 
     default:
       return "";
   }
